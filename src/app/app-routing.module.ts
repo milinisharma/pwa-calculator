f597150b958cd154cb57c68e4b60e26f9998c2ee
milinisharma <74365456+milinisharma@users.noreply.github.com>
2025-07-17 22:33:27 +0530

    Temp buttons and screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './components/calculator/calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: CalculatorComponent
+  },
+  {
+    path: "**", redirectTo: "", component: CalculatorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
